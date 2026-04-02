5d1f4be93964dd2ddf745ec3a67d7b245138fb27
Collin Snider <c-snider.3@onu.edu>
2026-04-02 09:03:17 -0400

    Completed isSortedIncreasing

--- P9.2/Source.cpp
+++ P9.2/Source.cpp
@@ -1,2 +1,41 @@
 /*Integer sort check - Collin Snider*/
+/*Write array functions that carry out the following tasks for a passed array of integers:
+• isSortedIncreasing: Returns true if the array is currently sorted in increasing order.
+• isSortedDecreasing: Returns true if the array is currently sorted in decreasing order.
+• hasAdjecentDuplicates: Returns true if the array contains two adjacent duplicate values.
+• hasDuplicates: Returns true if the array contains duplicate values (which need not be adjacent).*/
 
+#include <iostream>
+using namespace std;
+
+bool isSortedIncreasing(int arr[], int size);
+
+bool isSortedIncreasing(int arr[], int size) {
+	for (int i = 0; i < size - 1; i++) {
+		if (arr[i] > arr[i + 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+
+	const int size = 6;
+
+	int arr[size];
+	for (int i = 0; i < size; i++) {
+		cout << "Enter integer #" << i << ": ";
+		cin >> arr[i];
+	}
+
+
+	if (isSortedIncreasing(arr, size)) {
+		cout << "The data are increasing." << endl;
+	}
+	else {
+		cout << "The data are not increasing." << endl;
+	}
+
+
+}
